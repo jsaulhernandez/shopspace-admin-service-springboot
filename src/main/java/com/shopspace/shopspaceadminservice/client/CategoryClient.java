215a0c7b8a3276cbbc457e4fae54ccb86fd258eb
Sony <jsaulhernandez98@gmail.com>
2023-07-04 18:15:17 -0600

    tag: adding fallback factory to category client

--- src/main/java/com/shopspace/shopspaceadminservice/client/CategoryClient.java
+++ src/main/java/com/shopspace/shopspaceadminservice/client/CategoryClient.java
@@ -1,14 +1,14 @@
 package com.shopspace.shopspaceadminservice.client;
 
+import com.shopspace.shopspaceadminservice.client.fallback.CategoryFallback;
 import com.shopspace.shopspaceadminservice.dto.CategoryDTO;
 import com.shopspace.shopspaceadminservice.dto.pageable.PageDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "${shopspace.feign.config.name}", url = "${shopspace.feign.config.url}")
+@FeignClient(value = "${shopspace.feign.config.name}", url = "${shopspace.feign.config.url}", fallbackFactory = CategoryFallback.class)
 public interface CategoryClient {
     @GetMapping("/category/paged")
-    PageDTO<CategoryDTO[]> getAllPagedCategories(@RequestParam String search, @RequestParam Integer page,
-            @RequestParam Integer size);
+    PageDTO<CategoryDTO[]> getAllPagedCategories(@RequestParam String search, @RequestParam Integer page, @RequestParam Integer size);
 }
---
+++ src/main/java/com/shopspace/shopspaceadminservice/client/fallback/CategoryFallback.java
@@ -0,0 +1,28 @@
+package com.shopspace.shopspaceadminservice.client.fallback;
+
+import com.shopspace.shopspaceadminservice.client.CategoryClient;
+import com.shopspace.shopspaceadminservice.dto.CategoryDTO;
+import com.shopspace.shopspaceadminservice.dto.pageable.PageDTO;
+import feign.FeignException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryFallback implements FallbackFactory<CategoryClient> {
+    Logger logger = LoggerFactory.getLogger(CategoryFallback.class);
+
+    @Override
+    public CategoryClient create(Throwable cause) {
+        logger.error("An exception occurred when calling the CategoryClient", cause);
+
+        return new CategoryClient() {
+            @Override
+            public PageDTO<CategoryDTO[]> getAllPagedCategories(String search, Integer page, Integer size) {
+                logger.error("[Fallback] not call getAllPagedCategories");
+                return null;
+            }
+        };
+    }
+}
