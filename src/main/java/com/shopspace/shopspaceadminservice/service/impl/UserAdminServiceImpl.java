ea921c777924cb268cc12a65029f57f770f52ff3
Sony <jsaulhernandez98@gmail.com>
2023-07-11 17:37:19 -0600

    fix: adding annotation @Service to impl for user admin

--- src/main/java/com/shopspace/shopspaceadminservice/service/impl/UserAdminServiceImpl.java
+++ src/main/java/com/shopspace/shopspaceadminservice/service/impl/UserAdminServiceImpl.java
@@ -9,9 +9,11 @@ import com.shopspace.shopspaceadminservice.service.UserAdminService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class UserAdminServiceImpl implements UserAdminService {
     Logger logger = LoggerFactory.getLogger(UserAdminServiceImpl.class);
     @Autowired
