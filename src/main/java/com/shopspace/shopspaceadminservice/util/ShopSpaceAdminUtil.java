413ecee5fb33c8df0ad68f4c0af908a51ed03770
Sony <jsaulhernandez98@gmail.com>
2023-07-13 12:04:05 -0600

    tag: adding new utils
    Utils for get ip and user agent

--- src/main/java/com/shopspace/shopspaceadminservice/util/ShopSpaceAdminUtil.java
+++ src/main/java/com/shopspace/shopspaceadminservice/util/ShopSpaceAdminUtil.java
@@ -1,4 +1,23 @@
 package com.shopspace.shopspaceadminservice.util;
 
+import com.google.common.net.HttpHeaders;
+import jakarta.servlet.http.HttpServletRequest;
+
 public class ShopSpaceAdminUtil {
+    public static String getClientIp(HttpServletRequest request) {
+        var remoteAddr = "";
+
+        if (request != null) {
+            remoteAddr = request.getHeader(HttpHeaders.X_FORWARDED_FOR);
+            if (remoteAddr == null || "".equals(remoteAddr)) remoteAddr = request.getRemoteAddr();
+        }
+
+        return remoteAddr;
+    }
+
+    public static String getUserAgent(HttpServletRequest request) {
+        var ua = "";
+        if (request != null) ua = request.getHeader(HttpHeaders.USER_AGENT);
+        return ua;
+    }
 }
