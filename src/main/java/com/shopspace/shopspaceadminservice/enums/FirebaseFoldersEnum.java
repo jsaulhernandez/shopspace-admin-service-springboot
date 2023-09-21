4008c3674a4cbfcc097dcab46d86e06b95070247
Sony <jsaulhernandez98@gmail.com>
2023-09-21 17:16:14 -0600

    tag: adding new folders to firebase

--- src/main/java/com/shopspace/shopspaceadminservice/enums/FirebaseFoldersEnum.java
+++ src/main/java/com/shopspace/shopspaceadminservice/enums/FirebaseFoldersEnum.java
@@ -2,4 +2,6 @@ package com.shopspace.shopspaceadminservice.enums;
 
 public class FirebaseFoldersEnum {
     public static final String PRODUCTS = "products";
+    public static final String CATEGORIES = "categories";
+    public static final String BRANDS = "brands";
 }
