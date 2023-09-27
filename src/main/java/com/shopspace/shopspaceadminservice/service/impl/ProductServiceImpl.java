506bf7d08e7b341c38467fb08b3d0e4fa4d89e21
Sony <jsaulhernandez98@gmail.com>
2023-09-27 16:29:48 -0600

    tag: adding method to remove files from firebase storage

--- src/main/java/com/shopspace/shopspaceadminservice/service/impl/ProductServiceImpl.java
+++ src/main/java/com/shopspace/shopspaceadminservice/service/impl/ProductServiceImpl.java
@@ -5,7 +5,6 @@ import com.shopspace.shopspaceadminservice.dto.ProductDTO;
 import com.shopspace.shopspaceadminservice.dto.ViewProductDTO;
 import com.shopspace.shopspaceadminservice.dto.pageable.PageDTO;
 import com.shopspace.shopspaceadminservice.enums.FirebaseFoldersEnum;
-import com.shopspace.shopspaceadminservice.exception.ConflictException;
 import com.shopspace.shopspaceadminservice.exception.DataNotFoundException;
 import com.shopspace.shopspaceadminservice.service.FileService;
 import com.shopspace.shopspaceadminservice.service.ProductService;
@@ -14,6 +13,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -30,11 +31,12 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductDTO create(ProductDTO productDTO){
+        logger.info("processing file to create product {}", productDTO.getTitle());
+
         var views = productDTO.getViewProducts().stream().peek(vp -> {
-            logger.info("upload base64 to {}", productDTO.getTitle());
             var identifier = fileService.upload(vp.getImage(), FirebaseFoldersEnum.PRODUCTS);;
             vp.setImage(identifier);
-            logger.info("identifier to product {} is {}", productDTO.getTitle(), identifier);
+            logger.info("new file is {}", identifier);
         }).toList();
 
         productDTO.setViewProducts(views);
@@ -50,12 +52,18 @@ public class ProductServiceImpl implements ProductService {
 
         productDTO.setId(oldProduct.get().getId());
 
+        logger.info("processing file to update product {}", productDTO.getTitle());
+
+        //cleaning firebase storage
+        this.cleanFirebaseStorage(oldProduct.get().getViewProducts(), productDTO.getViewProducts());
+
         //validation to images
-        var oldViews = oldProduct.get().getViewProducts();
         var views = productDTO.getViewProducts().stream().peek(vp -> {
             // sometime some views doesn't have id, for this case the var must have null
             ViewProductDTO oldView = null;
-            if (vp.getId() != null) oldView = oldViews.stream().filter(ovp -> ovp.getId().longValue() == vp.getId().longValue()).findFirst().orElse(null);
+            if (vp.getId() != null) {
+                oldView = oldProduct.get().getViewProducts().stream().filter(ovp -> ovp.getId().longValue() == vp.getId().longValue()).findFirst().orElse(null);
+            }
 
             if (vp.getImage().contentEquals("default")) {
                 //setting value of Data Base
@@ -66,15 +74,14 @@ public class ProductServiceImpl implements ProductService {
             } else {
                 if (oldView != null) {
                     //removing old image
-                    logger.info("removing file to {}: {}", productDTO.getTitle(), oldView.getImage());
+                    logger.info("removing file to {}, because have base64", oldView.getImage());
                     fileService.delete(ShopSpaceAdminUtil.encode(oldView.getImage()));
                 }
 
                 //saving new image
-                logger.info("upload base64 to {}", productDTO.getTitle());
                 var identifier = fileService.upload(vp.getImage(), FirebaseFoldersEnum.PRODUCTS);;
                 vp.setImage(identifier);
-                logger.info("identifier to product {} is {}", productDTO.getTitle(), identifier);
+                logger.info("new file is {}", identifier);
             }
         }).toList();
 
@@ -90,11 +97,25 @@ public class ProductServiceImpl implements ProductService {
         if (product.isEmpty()) throw new DataNotFoundException("Product to deleted doesn't exists.");
 
         //removing images
+        logger.info("processing file to delete product {}", product.get().getTitle());
         product.get().getViewProducts().forEach(vp -> {
-            logger.info("removing file to {}: {}", product.get().getTitle(), vp.getImage());
+            logger.info("removing file {}", vp.getImage());
             fileService.delete(ShopSpaceAdminUtil.encode(vp.getImage()));
         });
 
         return productClient.delete(id);
+    }
+
+    //method to delete unnecessary files
+    public void cleanFirebaseStorage(List<ViewProductDTO> oldData, List<ViewProductDTO> newData){
+        //records with id
+        var recordIds = newData.stream().filter(vp -> vp.getId() != null).toList();
+        //getting records that exists in old data array to remove images
+        var deleteFiles = oldData.stream().filter(ovp -> recordIds.stream().noneMatch(evp -> evp.getId().longValue() == ovp.getId().longValue())).toList();
+
+        deleteFiles.forEach(dvp -> {
+            logger.info("removing path {} to view with id {}", dvp.getImage(), dvp.getId());
+            fileService.delete(ShopSpaceAdminUtil.encode(dvp.getImage()));
+        });
     }
 }
